862d3b734aea36a74dc4124cac5eaed44cbfe187
João Pedro <jpedro.dev@protom.me>
2024-05-02 21:28:07 -0300

    init

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Authentication.BearerToken;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace ApiTokenBearer.Controllers
+{
+    public class AuthController : ControllerBase
+    {
+        [HttpGet("/login")]
+        public IActionResult Login(string username, string password)
+        {
+            if (IsValidUser(username, password))
+            {
+                var claimsPrincipal = new ClaimsPrincipal(
+                 new ClaimsIdentity(
+                    new[] {new Claim(ClaimTypes.Name, username) },
+                    BearerTokenDefaults.AuthenticationScheme
+                 )
+                );
+
+                return SignIn(claimsPrincipal);
+            }
+
+            return Unauthorized();
+        }
+
+        private bool IsValidUser(string username, string password)
+        {
+            return username == "admin" && password == "admin";
+        }
+
+        [HttpGet("/user")]
+        [Authorize]
+        public IActionResult GetUser()
+        {
+            var user = User;
+            if (user?.Identity?.IsAuthenticated ?? false)
+            {
+                return Ok(user.Identity.Name);
+            }
+
+            return Unauthorized();
+        }
+    }
+}
